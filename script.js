479953d302f52bc0541b30fa1ebd686f5363d718
SankarEganthan <106478532+SankarEganthan@users.noreply.github.com>
2024-03-06 22:45:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,52 +4,52 @@ import { API_URL, EMAIL_KEY, KEY } from "./config.js";
 import { emailjs } from "./email.min.js";
 
 
-const switchers = [...document.querySelectorAll(".switcher")];
+// const switchers = [...document.querySelectorAll(".switcher")];
 
-switchers?.forEach((item) => {
-  item.addEventListener("click", function () {
-    switchers.forEach((item) =>
-      item.parentElement.classList.remove("is-active")
-    );
-    this.parentElement.classList.add("is-active");
-  });
-});
+// switchers?.forEach((item) => {
+//   item.addEventListener("click", function () {
+//     switchers.forEach((item) =>
+//       item.parentElement.classList.remove("is-active")
+//     );
+//     this.parentElement.classList.add("is-active");
+//   });
+// });
 
-let email, mobileNo, adhaarNo;
+// let email, mobileNo, adhaarNo;
 
 // Initialize Supabase client
 const supabase = createClient(API_URL, KEY);
 
-const form = document.getElementById("signupForm");
+// const form = document.getElementById("signupForm");
 
-form?.addEventListener("submit", (event) => {
-  event.preventDefault(); // Prevent default form submission
+// form?.addEventListener("submit", (event) => {
+//   event.preventDefault(); // Prevent default form submission
 
-  // Collect form data
-  email = document.getElementById("signup-email").value;
-  mobileNo = document.getElementById("mobile-no").value;
-  adhaarNo = document.getElementById("adhaar-no").value;
+//   // Collect form data
+//   email = document.getElementById("signup-email").value;
+//   mobileNo = document.getElementById("mobile-no").value;
+//   adhaarNo = document.getElementById("adhaar-no").value;
 
-  // Validate form data
-  if (!validateEmail(email)) {
-    alert("Please enter a valid email address");
-    return;
-  }
-  if (!validateMobileNo(mobileNo)) {
-    alert("Please enter a valid mobile number");
-    return;
-  }
-  if (!validateAdhaarNo(adhaarNo)) {
-    alert("Please enter a valid Aadhaar number");
-    return;
-  }
+//   // Validate form data
+//   if (!validateEmail(email)) {
+//     alert("Please enter a valid email address");
+//     return;
+//   }
+//   if (!validateMobileNo(mobileNo)) {
+//     alert("Please enter a valid mobile number");
+//     return;
+//   }
+//   if (!validateAdhaarNo(adhaarNo)) {
+//     alert("Please enter a valid Aadhaar number");
+//     return;
+//   }
 
-  sessionStorage.emailID = email;
-  sessionStorage.mobileNo = mobileNo;
-  sessionStorage.adhaarNo = adhaarNo;
-  // If all data is valid, proceed to the registration page
-  window.location.href = "registrationpage.html"; // Redirect to registration page
-});
+//   sessionStorage.emailID = email;
+//   sessionStorage.mobileNo = mobileNo;
+//   sessionStorage.adhaarNo = adhaarNo;
+//   // If all data is valid, proceed to the registration page
+//   window.location.href = "registrationpage.html"; // Redirect to registration page
+// });
 
 function validateEmail(email) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
@@ -69,11 +69,33 @@ const form_registration_submit = document.getElementById(
 
 form_registration_submit?.addEventListener("submit", async (event) => {
   event.preventDefault(); // Prevent default form submission
-  const email = sessionStorage.emailID;
-  const mobileNo = sessionStorage.mobileNo;
-  const adhaarNo = sessionStorage.adhaarNo;
+
+  // Collect form data
+  const email = document.getElementById("signup-email").value;
+  const mobileNo = document.getElementById("mobile-no").value;
+  const adhaarNo = document.getElementById("adhaar-no").value;
+
+  // Validate form data
+  if (!validateEmail(email)) {
+    alert("Please enter a valid email address");
+    return;
+  }
+  if (!validateMobileNo(mobileNo)) {
+    alert("Please enter a valid mobile number");
+    return;
+  }
+  if (!validateAdhaarNo(adhaarNo)) {
+    alert("Please enter a valid Aadhaar number");
+    return;
+  }
+  // const email = sessionStorage.emailID;
+  // const mobileNo = sessionStorage.mobileNo;
+  // const adhaarNo = sessionStorage.adhaarNo;
   // Collect form data
   const voterID = document.getElementById("voter-ID").value;
+  const userName = document.getElementById("user-name").value;
+  const userAge = document.getElementById("user-age").value;
+  const userGender = document.getElementById("gender").value;
   const address = document.getElementById("address").value;
   const district = document.getElementById("district").value;
   const state = document.getElementById("state").value;
@@ -88,6 +110,11 @@ form_registration_submit?.addEventListener("submit", async (event) => {
     return;
   }
 
+  if(!(userAge>=18)){
+    alert("Your Age is lesser than 18, Register once your age is equal to or greater than 18 years");
+    return;
+  }
+
   // Validate form data if necessary
 
   try {
@@ -99,6 +126,9 @@ form_registration_submit?.addEventListener("submit", async (event) => {
           mobile_no: mobileNo,
           adhaar_no: adhaarNo,
           voter_id: voterID,
+          user_name: userName,
+          user_age: userAge,
+          user_gender: userGender,
           address: address,
           district: district,
           state: state,
@@ -148,7 +178,7 @@ loginForm?.addEventListener("submit", async (event) => {
 
     if (!userData) {
       // If no user found with this email
-      alert("Invalid email");
+      alert("Invalid User, Please register by clicking New User");
       return;
     }
 
@@ -292,6 +322,9 @@ async function submitVote(userID) {
         user_email_id: userData.email_id,
         user_mobile_no: userData.mobile_no,
         user_adhaar_no: userData.adhaar_no,
+        user_name: userData.user_name,
+        user_age: userData.user_age,
+        user_gender: userData.user_gender,
         user_voter_id: userData.voter_id,
         user_address: userData.address,
         user_district: userData.district,
@@ -317,7 +350,7 @@ async function submitVote(userID) {
     alert("Vote saved successfully");
     emailjs.init(EMAIL_KEY); 
     emailjs.send("service_nrmcgha","template_zjj5jzk",{
-      to_name: userData.email_id,
+      to_name: userData.user_name,
       from_name: "Election Commission",
       message: "Your vote successfully submitted to " + candidateData.candidate_name,
       to_email: userData.email_id,
@@ -354,3 +387,71 @@ const logOutElement = document.getElementById("log-out-button-id");
 logOutElement?.addEventListener("click", function () {
   window.location.href = "index.html";
 });
+
+const dashboardElement = document.getElementById("dashboard");
+dashboardElement?.addEventListener("click", function () {
+  window.location.href = "dashboardpage.html";
+});
+
+// Function to fetch and display all candidate data
+async function displayAllCandidates() {
+  try {
+    const { data: candidates, error } = await supabase
+      .from("ElectrolCandidates")
+      .select("*")
+      .order("id");
+
+    if (error) {
+      throw error;
+    }
+
+    if (!candidates || candidates.length === 0) {
+      // Handle case where no candidates are found
+      console.log("No candidates found");
+      return;
+    }
+
+    const candidatesContainer = document.getElementById("candidates-container-dashboard");
+
+    // Clear previous content if any
+    candidatesContainer.innerHTML = "";
+
+    // // Filter candidates based on user's district and state
+    // const filteredCandidates = candidates.filter((candidate) => {
+    //   return (
+    //     candidate.district.toLowerCase() === userDistrict &&
+    //     candidate.state.toLowerCase() === userState
+    //   );
+    // });
+
+    // Iterate through candidates and display information
+    candidates.forEach((candidate, index) => {
+      const candidateDiv = document.createElement("div");
+      candidateDiv.classList.add("voters-card");
+
+      // Display candidate information
+      candidateDiv.innerHTML = `<div class="voters-info">
+      <div>
+      <img src="${candidate.avatar}" alt="${candidate.candidate_name}" class="avatar" />
+      </div>
+      <div class="voter-details">
+      <p>Name: <span class="non-bold">${candidate.candidate_name}</span></p>
+      <p>Party: <span class="non-bold">${candidate.party_name}</span></p>
+      <p>District: <span class="non-bold">${candidate.district}</span></p>
+      <p>State: <span class="non-bold">${candidate.state}</span></p>
+      </div>
+      </div>
+          <input type="radio" name="candidate" value="${candidate.id}" id="candidate-${index}">
+      </div>`;
+
+      candidatesContainer.appendChild(candidateDiv);
+    });
+  } catch (error) {
+    console.error("Error fetching candidates:", error);
+  }
+}
+const candidatesContainerDashboard= document.getElementById("candidates-container-dashboard");
+
+if (candidatesContainerDashboard) {
+  displayAllCandidates();
+}
